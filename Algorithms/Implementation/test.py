72ab8ca790ae4dfaea2c023960c17e00465bc8ad
Park-Minjoo <alswn9030@gmail.com>
2024-02-15 17:16:13 +0900

    [UPDATE] Global Warming

--- Algorithms/Implementation/test.py
+++ Algorithms/Implementation/test.py
@@ -1,35 +1,5 @@
 row, col = map(int, input().split())
-arr = [list(input().split()) for _ in range(row)]
-new = [ ['.' for i in range(col)] for j in range(row)]
-# print(new)
-
-steps = [(-1, 0), (0, -1), (1,0), (0, 1)]
-
-
-
-for i in range(row):
-    for j in range(col):
-        if arr[i][j] == 'X':
-            # print(i, j, arr[i][j])
-            count = 0
-            for step in steps:
-                next_row = i + step[0]
-                next_col = j + step[1]
-                # print(next_row, next_col, arr[next_row][next_col])
-                if arr[next_row][next_col] == '.' or \
-                        next_row < 0 or next_col < 0 or \
-                        next_row > row or next_col > col:
-                    count += 1
-            # print(count)
-            if count < 3:
-                # print(new[i][j], arr[i][j])
-                new[i][j] = 'X'
-
-print('arr')
-for i in range(row):
-    print(arr[i])
-
-print('new')
-for i in range(row):
-    print(new[i])
+arr = []
+arr.append(list(input()) for _ in range(row))  # input 지도
+print(arr)
 #%%
--- Algorithms/test.py
+++ Algorithms/test.py
@@ -1,11 +1,54 @@
-user_input = input("문자열을 입력하세요: ")
-# 문자열을 대시('-')로 분리하여 단어들을 리스트로 만듭니다.
-words = user_input.split('-')
-acronym_list = [word[0].upper() for word in words]
+row, col = map(int, input().split())
+arr = [list(input()) for _ in range(row)] # input
+new = [] # new array
 
+# 방항 벡터
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-# 리스트의 각 요소를 합쳐서 새로운 문자열을 생성합니다.
-acronym = ''.join(acronym_list)
+for i in range(row):
+    for j in range(col):
+        if arr[i][j] == 'X': # 땅이면 인접한 세 칸이상이 바다인지 확인
+            count = 0 # 카운트는 X를 만날 때마다 초기화 되어야 함
+            for k in range(4): # 4개 다 확인하기
+                nx = i + dx[k]
+                ny = j + dy[k]
+                if 0 <= nx < row and 0 <= ny < col:
+                    if arr[nx][ny] == '.':
+                        count += 1
+                else:
+                    count += 1 # 범위를 벗어날 때
+                    continue
+            if count >= 3:
+                new.append((i, j))
 
-print(words)
-#%%
+# . 으로 바꾸기
+if len(new) > 0:
+    for x, y in new:
+        arr[x][y] = '.'
+
+x1 = 0
+y1 = col - 1
+x2 = 0
+y2 = 0
+
+# 지도 범위 구하기
+
+# x의 시작값
+for i in range(row):
+    if 'X' in arr[i]:
+        x1 = i
+        break
+
+# x의 마지막 값
+for i in range(row-1, -1, -1):
+    if 'X' in arr[i]:
+        x2 = i
+        break
+
+# y값 ----> ***이게 너무 어려움
+for i in range(x1, x2 + 1): # 1 -> .X.
+    for j in range(col): # 3
+        if arr[i][j] == 'X': # arr[2][1]
+            y1 = min(y1, j) # min(2, 1) = 1
+            y2 = max(y2, j) # max(0, 1) = 1
