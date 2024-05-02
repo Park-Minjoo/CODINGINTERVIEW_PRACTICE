d23caa89731559c706a0cc3de5fa79c4b5db9c52
Park-Minjoo <61863242+Park-Minjoo@users.noreply.github.com>
2024-05-02 09:45:13 +0900

    [Silver III] Title: 게으른 백곰, Time: 460 ms, Memory: 49820 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.py
@@ -1,19 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-N, K = map(int, input().split())
-numbers = [0] * 1000001
+n, k = map(int, input().split())
+numbers = [0] * 1000001 # 얼음 양동이
 end = 0
-for _ in range(N):
+for _ in range(n):
     g, x = map(int, input().split())
     numbers[x] = g
     end = max(end, x)
 
-step = 2 * K + 1
+step = 2*k + 1
 window = sum(numbers[:step])
 answer = window
 
 for i in range(step, end+1):
-    window += numbers[i] - numbers[i-step]
+    window += (numbers[i] - numbers[i-step])
     answer = max(answer, window)
 print(answer)
